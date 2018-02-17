3a9b1498282e96ae2bc3cbf93276512cbd54347b
pauluhn <paul.uhn@gmail.com>
2018-02-17 10:08:10 -0600

    2+3

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,34 @@
 pragma solidity ^0.4.11;
 
 contract Election {
-	string public candidate;
+	
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	mapping(uint => Candidate) public candidates;
+	uint public candidatesCount;
+
+	mapping(address => bool) public voters;
 
 	function Election() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote(uint _candidateId) public {
+		require(!voters[msg.sender]);
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		voters[msg.sender] = true;
+
+		candidates[_candidateId].voteCount++;
 	}
 }
